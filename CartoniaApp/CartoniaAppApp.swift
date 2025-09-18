42afe3a9918e957fed57cd101f010a37fc83e533
lewhub474 <lewmxt615@gmail.com>
2025-09-18 12:00:33 -0500

    Initial commit

---
+++ CartoniaApp/CartoniaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CartoniaAppApp.swift
+//  CartoniaApp
+//
+//  Created by Macky on 18/09/25.
+//
+
+import SwiftUI
+
+@main
+struct CartoniaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
